ff869812e6cd7d3da3bb24a1adaac8c1db055af6
gataev <email>
2022-04-05 14:21:20 +0300

    Мелкая ошибка в Index.js

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use(require('./routes'))
+app.use("/images", express.static(path.resolve(__dirname, "images")));
+app.use(require("./routes"));
 
 const connect = async () => {
-    try {
-      await mongoose.connect(process.env.MONGO_DB);
-      console.log('connecting MongoDB');
-      app.listen(process.env.PORT, () => {
-        console.log('server has been started on port');
-      });
-    } catch (e) {
-      console.log(e.message);
-    }
-  };
-  connect();
+  try {
+    await mongoose.connect(process.env.MONGO_DB);
+    console.log("connecting MongoDB");
+    app.listen(process.env.PORT, () => {
+      console.log("server has been started on port");
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+connect();
